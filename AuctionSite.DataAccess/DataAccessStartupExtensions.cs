66d222e000b2dd78e940b8a1e62ee2d566553a60
Fabian Domurad <fabian.domurad@billennium.pl>
2020-05-12 13:25:14 +0200

    62 Added empty method templates

--- AuctionSite.DataAccess/DataAccessStartupExtensions.cs
+++ AuctionSite.DataAccess/DataAccessStartupExtensions.cs
@@ -1,6 +1,17 @@
-﻿namespace AuctionSite.DataAccess
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionSite.DataAccess
 {
     public static class DataAccessStartupExtensions
     {
+        public static IServiceCollection DataAccessConfiguration(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection DatabaseConnectionConfiguration(this IServiceCollection services)
+        {
+            return services;
+        }
     }
 }
