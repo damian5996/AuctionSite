634374aa2f28a7d1d970e3f2ca8753db7c6f547e
Damian Jacyna <damian.jacyna@billennium.com>
2020-05-20 14:29:34 +0200

    Added BaseLogic and Custom BusinessLogicException

---
+++ AuctionSite.Shared/Exceptions/BusinessLogicException.cs
@@ -0,0 +1,33 @@
+﻿using AuctionSite.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace AuctionSite.Shared.Exceptions
+{
+    public class BusinessLogicException : Exception
+    {
+        public ExceptionType ExceptionType;
+        public BusinessLogicException() : this(Constants.Error.Default)
+        {
+        }
+        public BusinessLogicException(string errorMessage) : this(errorMessage, ExceptionType.InternalServerError)
+        {
+        }
+
+        public BusinessLogicException(string errorMessage, ExceptionType exceptionType) : base(errorMessage)
+        {
+            ExceptionType = exceptionType;
+        }
+
+        public BusinessLogicException(Exception ex) : this(ex.Message, ExceptionType.InternalServerError, ex)
+        {
+        }
+
+        public BusinessLogicException(string errorMessage, ExceptionType exceptionType, Exception ex) : base(errorMessage, ex)
+        {
+            ExceptionType = exceptionType;
+        }
+    }
+}
