6d310fe2ab4f5b889f75e31eb9af50bfef6c2105
Damian Jacyna <damian.jacyna@billennium.com>
2020-05-20 19:04:10 +0200

    Added abstractions of BaseBusinessLogic

---
+++ AuctionSite.Bl/Common/IValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionSite.BL.Common
+{
+    internal interface IValidator<in TParam>
+    {
+        void Validate(TParam param);
+    }
+}
